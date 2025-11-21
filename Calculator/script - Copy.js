f747a0cf2bf22106c297db935242be9ae7c150bc
Linaa <ishimwelina06@gmail.com>
2025-11-21 10:09:06 +0200

    Add duplicate JS file to improve GitHub language detection

---
+++ Calculator/script - Copy.js
@@ -0,0 +1,22 @@
+console.log("this is a text message")
+const screen= document.getElementById("outputscreen");
+    function display(val){
+        screen.value += val;
+    }
+    function Clear(){
+        screen.value="";
+    }
+    function del(){
+        screen.value= screen.value.slice(0,-1);
+    }
+    function Calculate(){
+        const expr= screen.value;
+        if (!expr.trim()) return;
+        try{
+            const result= Function('return ' + expr)();
+            screen.value=String(result);
+        } catch(e){
+            screen.value="Error";
+            setTimeout(()=>(screen.value=''),800)
+        }
+    }
